2b27f36c0c88a8d887c579067fbb32d0993d1fe5
joaoaugustopc <joaoaugusto.jf@gmail.com>
2023-12-03 16:54:47 -0300

    add lista.js com os testes passando

---
+++ lista.js
@@ -0,0 +1,21 @@
+
+
+const lista = ["Um", "Dois", "Três"];
+
+export function getLista(item) {
+    return structuredClone(lista);
+}
+
+export function limpaLista() {
+    lista.splice(0, lista.length);
+}
+
+export function adicionaNaLista(item) {
+    lista.push(item);
+}
+
+export function removeDaLista(index) {
+    if (index >= 0 && index < lista.length) {
+        lista.splice(index, 1);
+    }
+}
